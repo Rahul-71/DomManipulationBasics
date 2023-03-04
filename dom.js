c1bfb6f9c197c7c07d7339e16580b3f0eb98e7a1
Ayush Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-03-04 17:26:45 +0530

    using getElementsByTagName to manipulate the tag without classname

--- dom.js
+++ dom.js
@@ -4,12 +4,17 @@ head.style.borderBottom = "solid 2px black";
 
 document.getElementById("header-title").innerHTML = "Welcome to the Item Lister";
 
-// getElementsByClassName
+// GETELEMENTSBYCLASSNAME
 let items = document.getElementsByClassName("list-group-item");
-// console.log(items);
 items[1].style.backgroundColor = "lightgreen";
 
 for (let i = 0; i < items.length; i++) {
     items[i].style.backgroundColor = "lightblue";
     items[i].style.fontWeight = 'bold';
+}
+
+// GETELEMENTSBYTAGNAME
+let items1 = document.getElementsByTagName("li");
+for (let i = 0; i < items1.length; i++) {
+    items1[i].style.backgroundColor = "tomato";
 }
