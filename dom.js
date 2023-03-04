78d75778dbbb635efc745343190df12c13e92cba
Ayush Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-03-04 17:17:01 +0530

    getElementsByClassName used on items list

--- dom.js
+++ dom.js
@@ -1,4 +1,15 @@
+// GETELEMENTBYID
 let head = document.getElementById("main-header");
 head.style.borderBottom = "solid 2px black";
 
 document.getElementById("header-title").innerHTML = "Welcome to the Item Lister";
+
+// getElementsByClassName
+let items = document.getElementsByClassName("list-group-item");
+// console.log(items);
+items[1].style.backgroundColor = "lightgreen";
+
+for (let i = 0; i < items.length; i++) {
+    items[i].style.backgroundColor = "lightblue";
+    items[i].style.fontWeight = 'bold';
+}
