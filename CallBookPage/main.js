d9da7f50f7b8e03076a097f266d44b94b9cba238
Ayush Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-03-07 11:32:58 +0530

    appointment tab added and will pick up user info from localStorage as well as show info of newly added customer

--- CallBookPage/main.js
+++ CallBookPage/main.js
@@ -1,7 +1,10 @@
 let submitBtn = document.querySelector('input[type="submit"]')
 
 submitBtn.addEventListener('click', saveInfo);
+const appointList = document.getElementById('appointmentTable');
+const appointBody = document.getElementById('appointmentBody');
 
+displayToAppointment();
 
 function saveInfo(e) {
     e.preventDefault();
@@ -30,4 +33,53 @@ function saveInfo(e) {
 
     localStorage.setItem("users", JSON.stringify(userArr));
     // console.log(localStorage);
+
+    // display info into appointment list
+    // create a row element first
+    const newRow = document.createElement('tr');
+    // create new cells
+    let nameTd = document.createElement('td');
+    nameTd.textContent = user.name;
+    let phoneTd = document.createElement('td');
+    phoneTd.textContent = user.contact;
+    let timeTd = document.createElement('td');
+    timeTd.textContent = user.time;
+
+    newRow.appendChild(nameTd);
+    newRow.appendChild(phoneTd);
+    newRow.appendChild(timeTd);
+
+    appointBody.appendChild(newRow);
+
+}
+
+
+function displayToAppointment() {
+    let userDetails = localStorage.getItem("users");
+
+    if (userDetails != null) {
+        let userArr = JSON.parse(userDetails);
+
+        userArr.forEach(user => {
+
+            // create a row element first
+            const newRow = document.createElement('tr');
+            // create new cells
+            let name = document.createElement('td');
+            name.textContent = user.name;
+            let phone = document.createElement('td');
+            phone.textContent = user.contact;
+            let time = document.createElement('td');
+            time.textContent = user.time;
+
+            newRow.appendChild(name);
+            newRow.appendChild(phone);
+            newRow.appendChild(time);
+
+
+            appointBody.appendChild(newRow);
+
+        });
+
+    }
 }
