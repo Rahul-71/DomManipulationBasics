150ff0d12d5d3b7ea60e76d970d96e716a3a7a31
Ayush Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-03-07 10:41:18 +0530

    storing user info as object to localStorage

--- CallBookPage/main.js
+++ CallBookPage/main.js
@@ -5,15 +5,29 @@ submitBtn.addEventListener('click', saveInfo);
 
 function saveInfo(e) {
     e.preventDefault();
+
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
     let phone = document.getElementById('phone').value;
     let time = document.getElementById('time').value;
 
-    localStorage.setItem("name", name);
-    localStorage.setItem("email", email);
-    localStorage.setItem("phone", phone);
-    localStorage.setItem("time", time);
 
+    const user = {
+        "name": name,
+        "email": email,
+        "contact": phone,
+        "time": time
+    };
+
+    let userArr = localStorage.getItem("users");
+
+    if (userArr == null) {
+        userArr = [];
+    } else {
+        userArr = JSON.parse(userArr);
+    }
+    userArr.push(user);
+
+    localStorage.setItem("users", JSON.stringify(userArr));
     // console.log(localStorage);
 }
