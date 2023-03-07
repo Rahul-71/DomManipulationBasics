e8f7bb8f99d0c9f9eee784f61d755e540209fc21
Ayush Tiwari <46418123+Rahul-71@users.noreply.github.com>
2023-03-08 02:11:32 +0530

    cancel call functionality & updating the localStorage as well

--- CallBookPage/main.js
+++ CallBookPage/main.js
@@ -4,6 +4,9 @@ submitBtn.addEventListener('click', saveInfo);
 const appointList = document.getElementById('appointmentTable');
 const appointBody = document.getElementById('appointmentBody');
 
+appointBody.addEventListener('click', deleteAppointment);
+
+
 displayToAppointment();
 
 function saveInfo(e) {
@@ -13,7 +16,6 @@ function saveInfo(e) {
     let email = document.getElementById('email').value;
     let phone = document.getElementById('phone').value;
     let time = document.getElementById('time').value;
-
 
     const user = {
         "name": name,
@@ -32,7 +34,6 @@ function saveInfo(e) {
     userArr.push(user);
 
     localStorage.setItem("users", JSON.stringify(userArr));
-    // console.log(localStorage);
 
     // display info into appointment list
     // create a row element first
@@ -45,12 +46,18 @@ function saveInfo(e) {
     let timeTd = document.createElement('td');
     timeTd.textContent = user.time;
 
+    let deleteTd = document.createElement('td');
+    let delbtn = document.createElement('button')
+    delbtn.classList = "btn btn-sm btn-danger cancelCall";
+    delbtn.textContent = 'X';
+    deleteTd.appendChild(delbtn);
+
     newRow.appendChild(nameTd);
     newRow.appendChild(phoneTd);
     newRow.appendChild(timeTd);
+    newRow.appendChild(deleteTd);
 
     appointBody.appendChild(newRow);
-
 }
 
 
@@ -72,14 +79,44 @@ function displayToAppointment() {
             let time = document.createElement('td');
             time.textContent = user.time;
 
+            let deleteTd = document.createElement('td');
+            let delbtn = document.createElement('button')
+            delbtn.classList = "btn btn-sm btn-danger cancelCall";
+            delbtn.textContent = 'X';
+            deleteTd.appendChild(delbtn);
+
             newRow.appendChild(name);
             newRow.appendChild(phone);
             newRow.appendChild(time);
-
+            newRow.appendChild(deleteTd);
 
             appointBody.appendChild(newRow);
 
         });
-
     }
+}
+
+// delete appointment while click of delete btn
+function deleteAppointment(e) {
+    e.preventDefault();
+
+    if (Array.from(e.target.classList).includes('cancelCall')) {
+        if (confirm("Are you sure to cancel the appointment ?")) {
+            // getting row of using del btn
+            const row = e.target.parentNode.parentNode;
+            // get the index of this row using 'rowIndex' property of table row
+            const rowIndx = row.rowIndex;
+
+            // fetch the userDetails from localStorage
+            let userDetails = localStorage.getItem("users");
+            userDetails = JSON.parse(userDetails);
+
+            userDetails.splice(rowIndx - 1, 1);
+
+            localStorage.setItem("users", JSON.stringify(userDetails));
+
+            row.remove();
+        }
+    }
+
 }
